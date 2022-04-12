98413e358f6c1c2abfb2aa2a96be5e6f34dec97b
jamiltarek55 <tarekjamil660@gmail.com>
2022-04-12 12:46:40 +0600

    first commit

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Chef.css';
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote_image" />
+          <p className="p__opensans">If you are a chef, no matter how good a chef you are.</p>
+        </div>
+        <p className="p__opensans">Whatever you do, do with determination. You have one life to live; do your work with passion and give your best. Whether you want to be a chef, doctor, actor, or a mother, be passionate to get the best result. </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign_image" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
